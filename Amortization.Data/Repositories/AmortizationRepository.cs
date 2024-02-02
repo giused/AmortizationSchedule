40020601ef89b614586fa403ec01f16c0ae6fb0a
Giuseppe Croce <giuseppe.croce@gmail.com>
2024-02-01 18:27:54 -0700

    Added Data Repository and test project

---
+++ Amortization.Data/Repositories/AmortizationRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amortization.Data.Repositories
+{
+    public class AmortizationRepository
+    {
+        public ApplicationDbContext DataContext { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AmortizationRepository"/> class.
+        /// </summary>
+        public AmortizationRepository() 
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AmortizationRepository"/> class.
+        /// </summary>
+        /// <param name="dataContext">The dataContext.</param>
+        public AmortizationRepository(ApplicationDbContext dataContext) 
+        { 
+            DataContext = dataContext;
+        }
+
+        public async Task SaveUser(User user)
+        {
+            if(user.UserId == 0)
+            {
+                DataContext.Entry(user).State = EntityState.Added;
+            }
+            else
+            {
+                DataContext.Entry(user).State = EntityState.Modified;
+            }
+            await DataContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Amortization.DataTests/Repositories/AmortizationRepositoryTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Amortization.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Amortization.Data.Repositories.Tests
+{
+    [TestClass()]
+    public class AmortizationRepositoryTests
+    {
+
+        protected ApplicationDbContext? DataContext { get; set; }
+        protected AmortizationRepository AmortizationRepository { get; set; }
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;Multiple Active Result Sets=True").Options;
+            DataContext = new ApplicationDbContext(options);
+            AmortizationRepository = new AmortizationRepository(DataContext);
+        }
+
+        [TestMethod()]
+        public async Task SaveUserTest()
+        {
+            User user = new User();
+            user.UserName = "Test"; 
+            await AmortizationRepository.SaveUser(user);
+            Assert.IsTrue(user.UserId > 0);
+        }
+    }
+}
