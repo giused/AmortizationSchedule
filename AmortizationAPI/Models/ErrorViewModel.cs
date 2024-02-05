0bc7ffceba5e63a13cf3ceb74c10e3d5f1528686
Giuseppe Croce <giuseppe.croce@gmail.com>
2024-02-05 12:33:27 -0700

    removing unnecessary projects

--- AmortizationAPI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AmortizationAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
